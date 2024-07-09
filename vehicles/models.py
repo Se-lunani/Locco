9e293e9293819f2ca34334199a1704a9bd90a55f
Se-lunani <mwamiselunani@gmail.com>
2024-07-09 22:36:45 +0300

    Added Booking models

--- vehicles/models.py
+++ vehicles/models.py
@@ -13,6 +13,22 @@ class Vehicles(models.Model):
 
     def __str__ (self):
         return f"{self.make} {self.model} ({self.year})"
+    
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey(Vehicles, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    total_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"Booking by {self.user.username} for {self.vehicle}"
+    
+
+
+
 
 
 
